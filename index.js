33f63b90250760412c7b1b2d2ddd39aa44923e1a
Michael Chiem <michaelchiem1@hotmail.com>
2024-03-03 17:59:00 -0700

    Updating and Fixed the index.js file

--- index.js
+++ index.js
@@ -46,3 +46,38 @@ inquirer.prompt(initialQuestion).then((answer) => {
 
     } else { return }
 })
+
+function addShape() {
+    inquirer.prompt(shapeQuestion).then((answer) => {
+
+        //console.log(answer);
+        let newShape;
+
+        if (answer.shapeChoice === 'Circle') {
+             newShape = new Circle(answer.shapeColor);
+        } else if (answer.shapeChoice === 'Square') {
+             newShape = new Square(answer.shapeColor);
+        } else {
+             newShape = new Triangle(answer.shapeColor);
+        }
+
+        newShape.setColor(answer.shapeColor);
+        const shapeElement = newShape.render();
+
+        const svgData =
+    `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+
+${shapeElement}
+
+<text x="150" y="150" font-size="${answer.fontSize}" text-anchor="middle" fill="${answer.shapeTextColor}">${answer.shapeText}</text>
+
+</svg>`;
+
+
+        fs.writeFileSync('logo.svg', svgData);
+
+    })
+
+
+
+}
